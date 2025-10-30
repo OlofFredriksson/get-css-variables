4c075c2b46d7b7f6dfc1b0dae5d4104832dbf5c0
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-30 14:03:11 +0100

    feat: add typescript declarations

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
 const css = require("@adobe/css-tools");
 
 /**
- * @param {*} String CSS source
- * @returns Js obect with variables
+ * @param {import("@adobe/css-tools").CssAtRuleAST} rule
+ * @returns {rule is import("@adobe/css-tools").CssRuleAST}
  */
-export function getVariables(source) {
-    const result = {};
-    css.parse(source)
-        .stylesheet.rules.filter(
-            (rule) =>
-                rule.selectors && rule.selectors.some((sel) => sel === ":root"),
-        )
-        .forEach((rule) =>
-            rule.declarations
-                .filter(
-                    (decl) =>
-                        decl.type === "declaration" &&
-                        decl.property.indexOf("--") === 0,
-                )
-                .forEach((decl) => (result[decl.property] = decl.value)),
-        );
-    return result;
+function isRootRule(rule) {
+    return rule.type === "rule" && rule.selectors.includes(":root");
 }
 
 /**
- *
- * @param {*} String CSS source
- * @returns Stringified object surounded with module.exports
+ * @param {import("@adobe/css-tools").CssCommentAST | import("@adobe/css-tools").CssDeclarationAST} decl
+ * @returns {decl is import("@adobe/css-tools").CssDeclarationAST}
+ */
+function isVariableDeclaration(decl) {
+    return decl.type === "declaration" && decl.property.startsWith("--");
+}
+
+/**
+ * @param {import("@adobe/css-tools").CssDeclarationAST} decl
+ * @returns {[key: string, value: string]}
+ */
+function entry(decl) {
+    return [decl.property, decl.value];
+}
+
+/**
+ * @public
+ * @param {string} source - CSS source
+ * @returns {Record<string, string>} Js object with variables
+ */
+export function getVariables(source) {
+    const { stylesheet } = css.parse(source);
+    const rules = stylesheet.rules.filter(isRootRule);
+    const variables = rules.map((rule) => {
+        return rule.declarations.filter(isVariableDeclaration).map(entry);
+    });
+    return Object.fromEntries(variables.flat());
+}
+
+/**
+ * @public
+ * @param {string} source - CSS source
+ * @returns {string} Stringified object surounded with module.exports
  */
 export function moduleReturnVariables(source) {
     return `module.exports = ${JSON.stringify(getVariables(source))}`;
