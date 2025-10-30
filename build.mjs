69caddddca0f63cd5656589965fe6d007c5a5a2d
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-10-30 15:02:48 +0100

    build: use esbuild and bundle dependencies

---
+++ build.mjs
@@ -0,0 +1,26 @@
+import fs from "node:fs/promises";
+import { build, analyzeMetafile } from "esbuild";
+
+const extension = {
+    cjs: ".cjs",
+    esm: ".mjs",
+};
+
+await fs.rm("dist", { recursive: true, force: true });
+
+for (const format of ["cjs", "esm"]) {
+    const result = await build({
+        entryPoints: ["src/index.js"],
+        outdir: `dist/${format}`,
+        bundle: true,
+        format,
+        platform: 'node',
+        target: 'node20',
+        logLevel: "info",
+        metafile: true,
+        outExtension: {
+            ".js": extension[format],
+        },
+    });
+    console.log(await analyzeMetafile(result.metafile));
+}
